b4dc22f6c0aa41c906798dc391ea62798cf31acc
matt-farmer <solversurfer@icloud.com>
2019-03-06 16:11:46 +1100

    gql wrapper for websearch
    gql access to web searches, using learning area, subject, and arbitrary keywords.

--- resolvers.go
+++ resolvers.go
@@ -9,7 +9,12 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"strings"
 
 	graphql "github.com/playlyfe/go-graphql"
 )
@@ -21,6 +26,9 @@ func buildResolvers() map[string]interface{} {
 
 	resolvers := map[string]interface{}{}
 
+	//
+	// resolvers for sylabus queries
+	//
 	resolvers["syllabus/overview"] = func(params *graphql.ResolveParams) (interface{}, error) {
 		lookupKey := deriveKey(params)
 		return getJSONMap(lookupKey)
@@ -29,6 +37,49 @@ func buildResolvers() map[string]interface{} {
 	resolvers["syllabus/content"] = func(params *graphql.ResolveParams) (interface{}, error) {
 		lookupKey := deriveKey(params)
 		return getJSONMap(lookupKey)
+	}
+
+	//
+	// resolver for web-seach queries
+	//
+	resolvers["websearch/searchRequest"] = func(params *graphql.ResolveParams) (interface{}, error) {
+
+		searchTerms := deriveSearchTerms(params)
+		// log.Println("search terms: ", searchTerms)
+
+		const endpoint = "https://api.cognitive.microsoft.com/bing/v7.0/search"
+		token := "72754c1352f04b4f96f9a1b46d854257"
+
+		req, err := http.NewRequest("GET", endpoint, nil)
+		if err != nil {
+			log.Println(err)
+		}
+
+		param := req.URL.Query()
+		param.Add("q", searchTerms)
+		req.URL.RawQuery = param.Encode()
+
+		req.Header.Add("Ocp-Apim-Subscription-Key", token)
+
+		client := new(http.Client)
+		resp, err := client.Do(req)
+		if err != nil {
+			return nil, err
+		}
+
+		body, err := ioutil.ReadAll(resp.Body)
+		if err != nil {
+			return nil, err
+		}
+		defer resp.Body.Close()
+
+		ans := new(BingAnswer)
+		err = json.Unmarshal(body, &ans)
+		if err != nil {
+			return nil, err
+		}
+
+		return ans, nil
 	}
 
 	return resolvers
@@ -49,4 +100,58 @@ func deriveKey(params *graphql.ResolveParams) string {
 	// log.Println("lookup key is:\t", key)
 
 	return key
+}
+
+//
+// parses the parameters to construct a set of search terms to pass to the
+// web search engine
+//
+func deriveSearchTerms(params *graphql.ResolveParams) string {
+
+	// check if an input block was supplied
+	input_map, ok := params.Args["terms"].(map[string]interface{})
+	if !ok {
+		return ""
+	}
+
+	// now grab the variables passed
+	terms := make([]string, 0)
+	kws, ok := input_map["keywords"].([]interface{})
+	if ok {
+		for _, keyword := range kws {
+			terms = append(terms, keyword.(string))
+		}
+	}
+	learning_area, ok := input_map["learning_area"].(string)
+	if ok {
+		terms = append(terms, learning_area)
+	}
+	subject, ok := input_map["subject"].(string)
+	if ok {
+		terms = append(terms, subject)
+	}
+	stage, ok := input_map["stage"].(string)
+	if ok {
+		terms = append(terms, "stage "+stage)
+	}
+	course, ok := input_map["course_name"].(string)
+	if ok {
+		terms = append(terms, course)
+	}
+	content_area, ok := input_map["content_area"].(string)
+	if ok {
+		terms = append(terms, content_area)
+	}
+
+	// log.Println("terms: ", terms)
+
+	//make sure the search terms are within size tolerance for bing search
+	allTerms := strings.Join(terms, " ")
+	if len(allTerms) > 1024 {
+		safeTerms := allTerms[0:1023]
+		return safeTerms
+	} else {
+		return allTerms
+	}
+
 }
